c2a4bc2bd58ba611b92d6a6c60b7dccc5d8dafac
Sheickh <micigan806@gmail.com>
2026-06-16 12:34:07 +0300

    сделал

--- services/catalog-service/cmd/app/main.go
+++ services/catalog-service/cmd/app/main.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"catalog-service/internal/broker"
+	"catalog-service/internal/config"
+	"catalog-service/internal/models"
+	"catalog-service/internal/repository"
+	"catalog-service/internal/service"
+	"catalog-service/internal/transport"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	db := config.SetUpDatabaseConnection()
+
+	if err := db.AutoMigrate(
+		&models.Service{},
+	); err != nil {
+		log.Fatalf("не удалось выполнить миграции: %v", err)
+	}
+
+	kafkaCfg := config.NewKafkaConfig()
+
+	producer := broker.NewProducer(kafkaCfg, "services")
+	defer producer.Close()
+
+	serviceRepo := repository.NewServicesRepositry(db)
+	// producer := Вызов конструктора()
+
+	serivceService := service.NewServicesService(serviceRepo, producer)
+	// serivceService := service.NewServicesService(serviceRepo, producer)
+
+	router := gin.Default()
+	transport.RegisterRoutes(router, serivceService)
+	if err := router.Run(); err != nil {
+		log.Fatalf("не удалось запустить HTTP-сервер: %v", err)
+	}
+	router.Run(":8080")
+}
